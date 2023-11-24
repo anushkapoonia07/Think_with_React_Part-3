79beee7f5d034d8a20ab2291194efbee85f16097
anushkapoonia07 <141112216+anushkapoonia07@users.noreply.github.com>
2023-11-24 17:08:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="kalvium">
+      <h1>Hello Kalvium</h1>
+    </div>
+  )
+}
+
+export default App
+
